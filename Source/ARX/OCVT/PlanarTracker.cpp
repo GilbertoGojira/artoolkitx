780c81b0a3396f1187b8afad50534b16841cc05b
Gilberto <gilberto.catarino@gojira.tv>
2018-09-17 16:49:33 +0100

    Clear tracked marker count when deleting trackable

--- Source/ARX/OCVT/PlanarTracker.cpp
+++ Source/ARX/OCVT/PlanarTracker.cpp
@@ -512,6 +512,7 @@ public:
             _trackables[i].CleanUp();
         }
         _trackables.clear();
+        _currentlyTrackedMarkers = 0;
     }
     
     bool SaveTrackableDatabase(std::string fileName)
